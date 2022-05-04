5b6fd4e695ae4e6c910544f6f8e1fb10ea33962d
seba5197 <sebastian5197@gmail.com>
2022-05-03 23:34:20 -0400

    final 1

--- src/app.ts
+++ src/app.ts
@@ -10,18 +10,24 @@ app.use(express.json())
 
 
 //routes
+try {
+    app.use('/api/v1',apiRouter)
+} catch (error) {
+    app.use((_req, res) => {
 
-app.use('/api/v1',apiRouter)
+        res.status(404).json({
+            message: "error: "+error
+        })
+        
+        })
+}
+
 
 app.use((_req, res) => {
 
 res.status(404).json({
     message: "not found"
 })
-
-
-
-
 
 })
 
